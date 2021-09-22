f4c1d005979054841f2732937f4e454ff66e968a
root <root@ip-192-168-61-129.ap-northeast-2.compute.internal>
2021-09-22 12:08:31 +0800

    “1111111111111111111111333”

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,3 @@ cd /home/ec2-user/target
 echo "nohup java  -Xms256m -Xmx256m -jar app.jar --server.port=8080 >/home/ec2-user/consolelog/$jarname.log &"
 nohup  java  -Xms256m -Xmx256m -jar app.jar --server.port=8080>/home/ec2-user/consolelog/$jarname.log & 2>1 &
 echo "/home/ec2-user/consolelog/$jarname.log"
-
